f7d1a9020c39893c0067a3d4e24f1c10347717eb
lakhal84 <probusilakhal@gmail.com>
2022-03-08 23:48:49 +0100

    update

--- dxtdev-auth-app/src/firebase.js
+++ dxtdev-auth-app/src/firebase.js
@@ -9,20 +9,8 @@ const firebaseConfig = {
     storageBucket: process.env.REACT_APP__FIREBASE_STORAGEBUCKET,
     messagingSenderId: process.env.REACT_APP__FIREBASE_MESSAGINGSENDERID,
     appId: process.env.REACT_APP__FIREBASE_API_ID
-    //apiKey: "AIzaSyB8MJspiQXvEV_1HcjQJXe_GKVFvZYyf7k",
-    //authDomain: "dxtapp-dev.firebaseapp.com",
-    //projectId: "dxtapp-dev",
-    //storageBucket: "dxtapp-dev.appspot.com",
-    //messagingSenderId: "1044017737345",
-    //appId: "1:1044017737345:web:7a4c600977bfe4795b2780"
 }
 
-
-
-// Use this to initialize the firebase App
-//const firebaseApp= firebase.initializeApp(firebaseConfig)
-
-// Use these for db & auth
 initializeApp(firebaseConfig);
 
 export const auth = getAuth();
