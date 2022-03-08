f32fe48ec962b5140cc91e17f29f068b338e4f21
lakhal84 <probusilakhal@gmail.com>
2022-03-08 23:47:27 +0100

    update: firebase.js to use variables from env file instead firebase infos

--- dxtdev-auth-app/src/firebase.js
+++ dxtdev-auth-app/src/firebase.js
@@ -3,18 +3,18 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
-    // apiKey: process.env.EACT_FIREBASE_API_KEY,
-    // authDomain: process.env.REACT_FIREBASE_AUTHDOMAIN,
-    // projectId: process.env.REACT_FIREBASE_PROJECTID,
-    // storageBucket: process.env.REACT_FIREBASE_STORAGEBUCKET,
-    // messagingSenderId: process.env.REACT_FIREBASE_MESSAGINGSENDERID,
-    // appId: process.env.REACT_FIREBASE_API_ID
-    apiKey: "AIzaSyB8MJspiQXvEV_1HcjQJXe_GKVFvZYyf7k",
-    authDomain: "dxtapp-dev.firebaseapp.com",
-    projectId: "dxtapp-dev",
-    storageBucket: "dxtapp-dev.appspot.com",
-    messagingSenderId: "1044017737345",
-    appId: "1:1044017737345:web:7a4c600977bfe4795b2780"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_FIREBASE_AUTHDOMAIN,
+    projectId: process.env.REACT_APP__FIREBASE_PROJECTID,
+    storageBucket: process.env.REACT_APP__FIREBASE_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP__FIREBASE_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP__FIREBASE_API_ID
+    //apiKey: "AIzaSyB8MJspiQXvEV_1HcjQJXe_GKVFvZYyf7k",
+    //authDomain: "dxtapp-dev.firebaseapp.com",
+    //projectId: "dxtapp-dev",
+    //storageBucket: "dxtapp-dev.appspot.com",
+    //messagingSenderId: "1044017737345",
+    //appId: "1:1044017737345:web:7a4c600977bfe4795b2780"
 }
 
 
